b2c9f690bbf6f0cc2afc4da8352355ecd9a0587d
MioPortimo <t2pomi02@students.oamk.fi>
2024-09-15 14:20:11 +0300

    commit

---
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.bmi
+
+import android.os.Bundle
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bmi.ui.theme.BmiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BmiTheme {
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ){
+                        Bmi()
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Bmi(){
+    var heightInput: String by remember { mutableStateOf("")  }
+    var weightInput: String by remember { mutableStateOf("")  }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toFloatOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0) weigth/(height*height) else 0.0
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = "Body mass index",
+            fontsize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = {heightInput = it.replace(',',',')},
+            label = { Text("Height") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = {weightInput = it.replace(',',',')},
+            label = { Text("Weight") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = stringResource(R.string.result,String.format("%.2f",bmi).replace(',',',')))
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    BmiTheme {
+        Bmi()
+    }
+}
